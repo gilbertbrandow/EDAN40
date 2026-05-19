530c1c31301a2d42029e794858461cfceb0b0c2b
Gilbert Brandow <simon@0100.se>
2026-05-19 18:51:30 +0200

    Completed remaining statement executes

--- project_3/src/Statement.hs
+++ project_3/src/Statement.hs
@@ -1,55 +1,75 @@
 -- Leon Krasniqi and Simon Gustafsson
 
-module Statement(T, parse, toString, fromString, execute) where
-import Prelude hiding (return, fail)
+module Statement (T, parse, toString, fromString, execute) where
+
+import Dictionary qualified
+import Expr qualified
 import Parser hiding (T)
-import qualified Dictionary
-import qualified Expr
+import Prelude hiding (fail, return)
 
 type T = Statement
-data Statement =
-    Assignment String Expr.T 
-    | Skip 
-    | Begin [Statement]
-    | While Expr.T Statement
-    | Read Expr.T
-    | Write Expr.T 
-    | If Expr.T Statement Statement
-    deriving Show
+
+data Statement
+  = Assignment String Expr.T
+  | Skip
+  | Block [Statement]
+  | While Expr.T Statement
+  | Read String
+  | Write Expr.T
+  | If Expr.T Statement Statement
+  deriving (Show)
 
 assignment = word #- accept ":=" # Expr.parse #- require ";" >-> uncurry Assignment
 
 skip = accept "skip" -# require ";" >-> \_ -> Skip
 
-begin = accept "begin" -# iter parse #- require "end" >-> Begin
+begin = accept "begin" -# iter parse #- require "end" >-> Block
 
-while = accept "while" -# Expr.parse  #-  require "do" # parse >-> uncurry While
+while = accept "while" -# Expr.parse #- require "do" # parse >-> uncurry While
 
-readStmt = accept "read" -# Expr.parse #- require ";" >-> Read
+readStmt = accept "read" -# word #- require ";" >-> Read
 
 writeStmt = accept "write" -# Expr.parse #- require ";" >-> Write
 
-ifStmt = accept "if" -#  Expr.parse # (require "then" -# parse) # (require "else" -# parse) >-> uncurry (uncurry If)
+ifStmt = accept "if" -# Expr.parse # (require "then" -# parse) # (require "else" -# parse) >-> uncurry (uncurry If)
 
 class Executable t where
-    execute :: [t] -> Dictionary.T String Integer -> [Integer] -> [Integer]
+  execute :: [t] -> Dictionary.T String Integer -> [Integer] -> [Integer]
 
 instance Executable Statement where
-    -- execute :: [Statement] -> Dictionary.T String Integer -> [Integer] -> [Integer]
-    execute (Skip : stmts) dict input = execute stmts dict input
-    execute (Assignment name expr : stmts) dict input =
-        case (Expr.value expr dict) of
-            Left err -> error err
-            Right v -> execute stmts (Dictionary.insert (name, v) dict) input    
-    execute (If cond thenStmts elseStmts: stmts) dict input =
-        case (Expr.value cond dict) of
-            Left err -> error err
-            Right v ->
-                if v > 0 then
-                    execute (thenStmts: stmts) dict input
-                else
-                    execute (elseStmts: stmts) dict input
-
+  execute [] dict input = []
+  execute (Assignment name expr : stmts) dict input =
+    case (Expr.value expr dict) of
+      Left err -> error err
+      Right v -> execute stmts (Dictionary.insert (name, v) dict) input
+  execute (Skip : stmts) dict input = execute stmts dict input
+  execute (Block block : stmts) dict input =
+    execute (block ++ stmts) dict input
+  execute (While cond doStmt : stmts) dict input =
+    case (Expr.value cond dict) of
+      Left err -> error err
+      Right v ->
+        if v > 0
+          then
+            execute (doStmt : While cond doStmt : stmts) dict input
+          else
+            execute stmts dict input
+  execute (Read name : stmts) dict (inputVal : restInput) =
+    let newDict = Dictionary.insert (name, inputVal) dict
+     in execute stmts newDict restInput
+  execute (Write expr : stmts) dict input =
+    case Expr.value expr dict of
+      Left err -> error err
+      Right v -> v : execute stmts dict input
+  execute (If cond thenStmts elseStmts : stmts) dict input =
+    case (Expr.value cond dict) of
+      Left err -> error err
+      Right v ->
+        if v > 0
+          then
+            execute (thenStmts : stmts) dict input
+          else
+            execute (elseStmts : stmts) dict input
 
 instance Parse Statement where
   parse = assignment ! skip ! begin ! while ! readStmt ! writeStmt ! ifStmt
